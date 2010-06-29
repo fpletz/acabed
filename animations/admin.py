73ffdcd06f4136d6d6145fbd2f86d3823b559899
fpletz <fpletz@fnordicwalking.de>
2010-06-29 10:35:40 +0200

    update model

--- animations/admin.py
+++ animations/admin.py
@@ -1,8 +1,17 @@
-from animations.models import Frame, Animation, Playlist, Spool
+from animations.models import Animation, AnimationInstance, Playlist, SpoolJob
 from django.contrib import admin
 
-admin.site.register(Frame)
-admin.site.register(Animation)
-admin.site.register(Playlist)
-admin.site.register(Spool)
+class AnimationInstanceInline(admin.TabularInline):
+    model = AnimationInstance
+    extra = 1
+
+class AnimationAdmin(admin.ModelAdmin):
+    inlines = (AnimationInstanceInline,)
+
+class PlaylistAdmin(admin.ModelAdmin):
+    inlines = (AnimationInstanceInline,)
+
+admin.site.register(Animation, AnimationAdmin)
+admin.site.register(Playlist, PlaylistAdmin)
+admin.site.register(SpoolJob)
 
--- animations/models.py
+++ animations/models.py
@@ -1,54 +1,68 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Frame(models.Model):
-    nr = models.IntegerField()
-    duration = models.IntegerField()
-    data = models.TextField()
+def get_colors(self, height, width, depth, channels):
+    def nsplit(l, i):
+        return (l[:i], l[i:])
 
-    def __unicode__(self):
-        return '%i(%i): %s' % (self.nr, self.duration, self.data)
+    colors = []
 
-    def get_colors(self, height, width, depth, channels):
-        def nsplit(l, i):
-            return (l[:i], l[i:])
+    i = j = 0
+    while self.data != '':
+        l,self.data = nsplit(self.data, depth/16*channels)
+        colors.append(((i, j), l))
+        i += 1
+        if i == height:
+            j += 1
+            height = 0
 
-        colors = []
-
-        i = j = 0
-        while self.data != '':
-            l,self.data = nsplit(self.data, depth/16*channels)
-            colors.append(((i, j), l))
-            i += 1
-            if i == height:
-                j += 1
-                height = 0
-
-        return colors
+    return colors
 
 class Animation(models.Model):
+    type = models.CharField(max_length=1, choices=(('m', 'MOVIE'),('e', 'EXTERNAL')))
     title = models.CharField(max_length=128)
     description = models.TextField()
-    author = models.CharField(max_length=64)
+    author = models.CharField(max_length=512)
+    email = models.CharField(max_length=512)
+    creator = models.CharField(max_length=64)
+    user = models.ForeignKey(User)
     height = models.IntegerField()
     width = models.IntegerField()
     depth = models.IntegerField()
     channels = models.IntegerField()
-    frames = models.ManyToManyField('Frame')
+    max_duration = models.IntegerField()
+    data = models.TextField()
+    playlists = models.ManyToManyField('Playlist', through='AnimationInstance')
 
     def __unicode__(self):
-        return '%s(%i,%i,%i)' % (self.name, self.height, self.width, self.depth)
+        return '%s(%i,%i,%i)' % (self.title, self.height, self.width, self.depth)
 
-    def get_frames(self):
-        return self.frames.all().order_by('nr')
+class AnimationInstance(models.Model):
+    playlist = models.ForeignKey('Playlist')
+    animation = models.ForeignKey('Animation')
+    order = models.IntegerField()
+    playing = models.BooleanField()
 
 class Playlist(models.Model):
-    name = models.CharField(max_length=128)
-    animations = models.ManyToManyField('Animation')
+    title = models.CharField(max_length=128)
+    animations = models.ManyToManyField(Animation, through=AnimationInstance)
+    user = models.ForeignKey(User)
 
     def __unicode__(self):
-        return self.name
+        return self.title
 
-class Spool(models.Model):
-    playlists = models.ManyToManyField('Playlist')
+class SpoolJob(models.Model):
+    playlist = models.ForeignKey('Playlist')
+    priority = models.IntegerField()
     added = models.DateTimeField()
+
+    def __unicode__(self):
+        return '[' + self.playlist.title + '@' + str(self.priority) + '@' + str(self.added) + ']'
+
+    @staticmethod
+    def current():
+        try:
+            return SpoolJob.objects.order_by('priority', 'added')[0]
+        except IndexError:
+            return None
 
