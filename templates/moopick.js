e00568e3c45d42756835d9059fb1a4330dbaad39
fpletz <fpletz@fnordicwalking.de>
2010-07-05 23:11:42 +0200

    hack to fix non-intended color dragging behaviour

--- templates/moopick.js
+++ templates/moopick.js
@@ -90,8 +90,11 @@ var Moopick = new Class({
 					var li = new Element('li').setStyles(this.options.styles).set('html',hex).injectInside(this.pallet);
                                         var d = new Drag(li, {
                                             onComplete: function(el, ev) {
-                                                ev.target.setStyle('background-color', el.getStyle('background-color'));
-                                                ev.target.setProperty('html', el.getProperty('html'));
+                                                if(ev.target.parentNode.parentNode.id == 'colorpalette-tools')
+                                                {
+                                                    ev.target.setStyle('background-color', el.getStyle('background-color'));
+                                                    ev.target.setProperty('html', el.getProperty('html'));
+                                                }
                                             },
                                         });
                                     }
