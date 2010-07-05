02e1be757168185851ce72522b2193a042f10dd0
fpletz <fpletz@fnordicwalking.de>
2010-07-05 17:24:41 +0200

    agpl license foo

--- templates/movie.js
+++ templates/movie.js
@@ -1,3 +1,24 @@
+/*
+ *  acabed - webeditor for blinkenlights xml files
+ *  Copyright (C) 2010 Raphael Mancini <sepisultrum@hcl-club.lu>
+ *                     Franz Pletz <fpletz@fnordicwalking.de>
+ *
+ *  This file is part of acabed.
+ *
+ *  acabed is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  acabed is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var Movie = new Class({
     Implements: Events,
 
